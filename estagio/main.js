b10298c50898524ae6ea844b5858f467bcea93ac
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-03-08 15:45:44 -0300

    initial commit

---
+++ estagio/main.js
@@ -0,0 +1,32 @@
+//Primeiro exercício
+let i = 13;
+let soma = 0;
+let k = 0;
+
+while (k < i) {
+  k++;
+  soma += k;
+}
+console.log(soma);
+
+//Segundo exercício
+function fibonacci(numero) {
+  let a = 0;
+  let b = 1;
+
+  while (b <= numero) {
+    if (b === numero) {
+      console.log(`O número: ${numero}, pertence a sequência de Fibonacci.`);
+      return;
+    }
+
+    let temp = b;
+    b = a + b;
+    a = temp;
+  }
+
+  console.log(`O número: ${numero}, não pertence a sequência de Fibonacci.`);
+}
+fibonacci(90);
+
+//Terceiro exercício
